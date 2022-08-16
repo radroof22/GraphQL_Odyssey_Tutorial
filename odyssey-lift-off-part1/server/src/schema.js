05045e3faaa3029fa33d41d5f727804f58617685
Rohan Mehta <rmehta98@gatech.edu>
2022-08-16 00:25:54 -0400

    init

---
+++ odyssey-lift-off-part1/server/src/schema.js
@@ -0,0 +1,33 @@
+const {gql} = require("apollo-server")
+
+const typeDefs = gql`
+type Query {
+    "Query to get tracks array for the homepage grid"
+    tracksForHome: [Track!]!
+}
+
+"A track is a group of Modules that teaches a specific topic"
+type Track {
+    id: ID!
+    "track's title"
+    title: String!
+    "the track's main author"
+    author: Author!
+    "the track's main illustration to display in trck card or track page detail"
+    thumbnail: String
+    "the track's approximate length to complete, in minutes"
+    length: Int
+    "the number of modules this track contains"
+    modulesCount: Int
+}
+"Author of complete Track"
+type Author {
+    id: ID!
+    "Author's first and last name"
+    name: String!
+    "Author's profile picture url"
+    photo: String
+}
+`;
+
+module.exports = typeDefs;
