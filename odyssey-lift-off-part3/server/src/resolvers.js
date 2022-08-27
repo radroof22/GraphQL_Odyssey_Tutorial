0481c249a5768d0841cbe781d7adf9ffcc7994ad
Rohan Mehta <rmehta98@gatech.edu>
2022-08-27 01:01:22 -0400

    server complete

--- odyssey-lift-off-part3/server/src/resolvers.js
+++ odyssey-lift-off-part3/server/src/resolvers.js
@@ -4,11 +4,19 @@ const resolvers = {
     tracksForHome: (_, __, { dataSources }) => {
       return dataSources.trackAPI.getTracksForHome();
     },
+    // get a single track for the track api
+    track: (_, {id}, {dataSources}) => {
+      
+      return dataSources.trackAPI.getTrack(id)
+    }
   },
   Track: {
     author: ({ authorId }, _, { dataSources }) => {
       return dataSources.trackAPI.getAuthor(authorId);
     },
+    modules: ({id}, _, {dataSources}) => {
+      return dataSources.trackAPI.getTrackModules(id);
+    }
   },
 };
 
